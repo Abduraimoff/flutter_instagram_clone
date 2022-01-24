7f8ee163b3d19e99dc628f132f735974887b2749
Abduraimoff <abdurayimovmansur3@gmail.com>
2022-01-24 21:12:20 +0500

    .

--- lib/util/post.dart
+++ lib/util/post.dart
@@ -8,19 +8,19 @@ List posts = [
     'isLiked': true,
   },
   {
-    'name': 'Leo Messi',
-    'location': 'Paris',
-    'imageUrl': 'assets/images/messi.jpg',
-    'avatar': 'assets/images/story2.jpg',
-    'like': '34000 Likes',
-    'isLiked': true,
-  },
-  {
     'name': 'Tony Stark',
     'location': 'USA',
     'imageUrl': 'assets/images/tony.jpg',
     'avatar': 'assets/images/story4.jpg',
     'like': '90000 Likes',
     'isLiked': false,
+  },
+  {
+    'name': 'Leo Messi',
+    'location': 'Paris',
+    'imageUrl': 'assets/images/messi.jpg',
+    'avatar': 'assets/images/story2.jpg',
+    'like': '34000 Likes',
+    'isLiked': true,
   },
 ];
